f08803ae037d66ab5700ac6f3e9d19dfd7dae346
Heril Muratovic <hedzaprog@gmail.com>
2022-04-01 20:22:14 +0200

    Completed tests for product controller.

--- product-service/src/test/java/com/hedza06/springcloud/product/unit/controllers/ProductControllerTest.java
+++ product-service/src/test/java/com/hedza06/springcloud/product/unit/controllers/ProductControllerTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hedza06.springcloud.product.controllers.ProductController;
 import com.hedza06.springcloud.product.dto.ProductDTO;
 import com.hedza06.springcloud.product.entities.Product;
+import com.hedza06.springcloud.product.errors.CustomFeignClientErrorException;
+import com.hedza06.springcloud.product.errors.CustomInternalFeignClientException;
 import com.hedza06.springcloud.product.services.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.hedza06.springcloud.product.utils.TestUtils.getAllMocked;
 import static com.hedza06.springcloud.product.utils.TestUtils.getPersisted;
@@ -22,6 +26,7 @@ import static com.hedza06.springcloud.product.utils.TestUtils.getRequestProduct;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,6 +38,9 @@ public class ProductControllerTest {
 
     private static final String GET_ALL_API = "/product/all";
     private static final String CREATE_API  = "/product";
+    private static final String ASSIGN_TO_USER_API = "/product/assign-to-user/";
+    private static final String SIMULATE_INTERNAL_SERVER_ERROR_API = "/product/simulate/internal-server-error";
+    private static final String SIMULATE_BAD_REQUEST_ERROR_API = "/product/simulate/bad-request-error";
 
     @Autowired
     private MockMvc mockMvc;
@@ -99,5 +107,54 @@ public class ProductControllerTest {
                 .content(new ObjectMapper().writeValueAsBytes(productDTO))
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldAssignExistingProductToUser() throws Exception
+    {
+        String userEmail = "test@test.com";
+
+        ProductDTO productDTO = getRequestProduct();
+        productDTO.setId(1);
+
+        mockMvc
+            .perform(MockMvcRequestBuilders.put(ASSIGN_TO_USER_API + userEmail)
+                .content(new ObjectMapper().writeValueAsBytes(productDTO))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldReturnErrorSimulatingInternalServerCallToUserService() throws Exception
+    {
+        doThrow(
+            new CustomInternalFeignClientException("internal-server-error-msg", "request-url-test")
+        ).when(productService).simulateInternalServerError();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders.get(SIMULATE_INTERNAL_SERVER_ERROR_API))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("errorMessage").value("internal-server-error-msg"))
+            .andExpect(jsonPath("requestUrl").value("request-url-test"));
+    }
+
+    @Test
+    public void shouldReturnErrorSimulatingBadRequestCallToUserService() throws Exception
+    {
+        Map<String, Object> mockedErrorParams = new HashMap<>();
+        mockedErrorParams.put("msgFromClient", "Error occurred in data manipulation");
+        mockedErrorParams.put("statusFromClient", "DATA-ERROR");
+
+        doThrow(
+            new CustomFeignClientErrorException("bad-request-server-error-msg", "bad-request-url", mockedErrorParams)
+        ).when(productService).simulateBadRequestError();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders.get(SIMULATE_BAD_REQUEST_ERROR_API))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("errorMessage").value("bad-request-server-error-msg"))
+            .andExpect(jsonPath("requestUrl").value("bad-request-url"))
+            .andExpect(jsonPath("errorBody.msgFromClient").value("Error occurred in data manipulation"))
+            .andExpect(jsonPath("errorBody.statusFromClient").value("DATA-ERROR"));
     }
 }
